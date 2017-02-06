fdb1c5649b0239b14ade773d4ecc457fbe2d66b7
Agnieszka Burczyk <agnieszka.fuerst@gmail.com>
2017-02-06 19:06:53 +0100

    Added SQL queries

---
+++ sql queries/delete from tweet.sql
@@ -0,0 +1,62 @@
+delete from tweet
+where text like '%JEU%'
+or (text like '%blizzard%' and text like '%snow%')
+or (text like '%blizzard%' and text like '%Forza%')
+or (text like '%blizzard%' and text like '%PS4%')
+or (text like '%blizzard%' and text like '%giant%')
+or (text like '%blizzard%' and text like '%watch%')
+or (text like '%blizzard%' and text like '%weather%')
+or (text like '%blizzard%' and text like '%blitz%')
+or text like '%head to%'
+or text like '%head north%'
+or text like '%head south%'
+or text like '%head back%'
+or text like '%head home%'
+or text like '%in my head%'
+or text like '%inside my head%'
+or text like '%on my head%'
+or text like '%over my head%'
+or text like '%his head%'
+or text like '%her head%'
+or text like '%your head%'
+or text like '%head of%'
+or text like '%head up%'
+or text like '%head out%'
+or text like '%head on%'
+or text like '%head for%'
+or text like '%head over%'
+or text like '%head in%'
+or text like '%head injury%'
+or text like '%head hurts%'
+or text like '%head coach%'
+or (text like '%head%' and text like '%clear%')
+or (text like '%head%' and text like '%above%')
+or (text like '%head%' and text like '%shoulder%')
+or text like '%penis%'
+or text like '%viagra%'
+or text like '%wedding%'
+or text like '%abortion%'
+or text like '%nigga%'
+or text like '%fur%'
+or text like '%injured%'
+or text like '%dead%'
+or text like '%shoot%'
+or text like '%school%'
+or text like '%hairdressing%'
+or text like '%dandruff%'
+or text like '%merry christmas%'
+or text like '%merry xmas%'
+or text like '%Ralph Lauren%'
+or text like '%Mansion Elan%'
+or text like '%Hitler%'
+or text like '%Trump%'
+or text like '%warning%'
+or text like '%storm%'
+or text like '%winter%'
+or text like '%snowboard%'
+or text like '%anti-doping%'
+or text like '%star wars%'
+or text like '%football%'
+or text like '%snow cow%'
+or (text like '%wool%' and text like '%cap%');
+
